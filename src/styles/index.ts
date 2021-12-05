beed475d8ad0df76296c4f48e06ffa3126e4b363
Krsnananda <krsnananda@visie.com.br>
2021-12-05 18:58:35 -0300

    adiciona dependências e tela para lista vazia

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+`
+export const Logo = styled.Image`
+  width: 150px;
+  height: 150px;
+  margin-top: 15%;
+` 
+
+export const EmptyList = styled.Text`
+  font-size: 18px;
+  margin: 100px 0;
+  font-weight: bold;
+`
